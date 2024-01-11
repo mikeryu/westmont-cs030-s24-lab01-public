c1db401c45cf2ddc17cc7e23474730a84334c52a
Mike Ryu <mryu@westmont.edu>
2024-01-10 22:15:32 -0800

    Starter code for Lab 01.

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * Westmont College Spring 2024
+ * CS 030 Lab 01
+ *
+ * @author Assistant Professor Mike Ryu mryu@westmont.edu
+ * @author Boaty McBoatface bmcboatface@westmont.edu TODO: replace this with your info.
+ */
+
+// TODO: What should the name of the class be?
+public class NameMe {
+
+    // TODO: Declare a constant integer `COUNT`, and set it to a number you want.
+
+
+    /**
+     * Write the `main` method under this comment block.
+     */
+
+}
